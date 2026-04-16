2eff897111f7f95be721cc01e232f357218326b6
agkayster <ejike.chiboka@gmail.com>
2026-04-16 17:49:39 +0100

    implemented GET and POST requests for the matches api and database

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import {matchRouter} from './routes/matches.js';
 const app = express();
 const PORT = 8000;
 
-// middleware to read JSON content
+
+// middleware that allows express to read JSON content/data
 app.use(express.json());
 
+// backend home route
 app.get('/', (req, res) => {
     res.json({ message: 'Sportz App WebSockets server is running 🚀' });
 });
+
+// to get the list of matches
+app.use("/matches", matchRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
